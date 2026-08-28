63b0e56feda3e5ef809d605532f0a19e2f63670f
ejlee8250-beep <ejlee8250@gmail.com>
2026-08-28 17:50:24 +0900

    260828_swiper,책정보 추가

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+  <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@14.0.1/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper('.mySwiper', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    </script>
